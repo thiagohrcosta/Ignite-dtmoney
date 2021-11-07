3ce79d388cac4377630f8fd2f82b6ea968532628
Thiago Costa <thiagohrcosta86@gmail.com>
2021-11-07 12:15:50 -0300

    Adding google fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
-  const teste = () => {
-    console.log("teste")
-  }
+
   return (
     <div className="App">
-      <Title>Hello World</Title>
-      <button onClick={teste}>Clicar</button>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
