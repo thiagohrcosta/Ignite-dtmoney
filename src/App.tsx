167f33f169983e1c1aa7950bdfc9034f3792177d
Thiago Costa <thiagohrcosta86@gmail.com>
2021-11-07 11:39:28 -0300

    Adding styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
+  const teste = () => {
+    console.log("teste")
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
+      <button onClick={teste}>Clicar</button>
     </div>
   );
 }
