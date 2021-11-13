a2acd59d4587ea7fff493bc39daa6dd44bcf2f22
Thiago Costa <thiagohrcosta86@gmail.com>
2021-11-13 19:20:47 -0300

    Adding modal component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -24,12 +25,10 @@ export function App() {
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashobard />
 
-      <Modal
-          isOpen={isNewTransactionModalOpen} 
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
 
       <GlobalStyle />
     </>
