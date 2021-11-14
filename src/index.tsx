d7af1d955b706042f802a88fdd240305c8694fda
Thiago Costa <thiagohrcosta86@gmail.com>
2021-11-14 16:21:19 -0300

    Listing transactions and using seeds

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,30 @@ createServer({
     transaction: Model, 
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-11-10 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1300,
+          createdAt: new Date('2021-11-13 09:00:00'),
+        },
+      ],
+      
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
