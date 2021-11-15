83d66689cae0862526bca3247b5ab281c616c70d
Thiago Costa <thiagohrcosta86@gmail.com>
2021-11-15 10:15:41 -0300

    Data insertion finished using context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { Children, createContext, useEffect, useState, ReactNode } from 'react'
+import { createContext, useEffect, useState, ReactNode } from 'react'
 import { api } from './services/api';
 
 
@@ -28,7 +28,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -43,8 +43,17 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    })
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ]);
   }
 
   return (
